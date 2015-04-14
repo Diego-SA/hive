6b4388b13a7b354d4bfd2be637f2dc6eb1a13f45
Jason Dere <jdere@apache.org>
2015-04-14 20:47:29 +0000

    HIVE-10226: Column stats for Date columns not supported (Jason Dere, reviewed by Ashutosh Chauhan)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1673553 13f79535-47bb-0310-9956-ffa450edef68

--- metastore/src/gen/thrift/gen-javabean/org/apache/hadoop/hive/metastore/api/Date.java
+++ metastore/src/gen/thrift/gen-javabean/org/apache/hadoop/hive/metastore/api/Date.java
@@ -31,22 +31,22 @@ import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class OpenTxnsResponse implements org.apache.thrift.TBase<OpenTxnsResponse, OpenTxnsResponse._Fields>, java.io.Serializable, Cloneable {
-  private static final org.apache.thrift.protocol.TStruct STRUCT_DESC = new org.apache.thrift.protocol.TStruct("OpenTxnsResponse");
+public class Date implements org.apache.thrift.TBase<Date, Date._Fields>, java.io.Serializable, Cloneable {
+  private static final org.apache.thrift.protocol.TStruct STRUCT_DESC = new org.apache.thrift.protocol.TStruct("Date");
 
-  private static final org.apache.thrift.protocol.TField TXN_IDS_FIELD_DESC = new org.apache.thrift.protocol.TField("txn_ids", org.apache.thrift.protocol.TType.LIST, (short)1);
+  private static final org.apache.thrift.protocol.TField DAYS_SINCE_EPOCH_FIELD_DESC = new org.apache.thrift.protocol.TField("daysSinceEpoch", org.apache.thrift.protocol.TType.I64, (short)1);
 
   private static final Map<Class<? extends IScheme>, SchemeFactory> schemes = new HashMap<Class<? extends IScheme>, SchemeFactory>();
   static {
-    schemes.put(StandardScheme.class, new OpenTxnsResponseStandardSchemeFactory());
-    schemes.put(TupleScheme.class, new OpenTxnsResponseTupleSchemeFactory());
+    schemes.put(StandardScheme.class, new DateStandardSchemeFactory());
+    schemes.put(TupleScheme.class, new DateTupleSchemeFactory());
   }
 
-  private List<Long> txn_ids; // required
+  private long daysSinceEpoch; // required
 
   /** The set of fields this struct contains, along with convenience methods for finding and manipulating them. */
   public enum _Fields implements org.apache.thrift.TFieldIdEnum {
-    TXN_IDS((short)1, "txn_ids");
+    DAYS_SINCE_EPOCH((short)1, "daysSinceEpoch");
 
     private static final Map<String, _Fields> byName = new HashMap<String, _Fields>();
 
@@ -61,8 +61,8 @@ public class OpenTxnsResponse implements org.apache.thrift.TBase<OpenTxnsRespons
      */
     public static _Fields findByThriftId(int fieldId) {
       switch(fieldId) {
-        case 1: // TXN_IDS
-          return TXN_IDS;
+        case 1: // DAYS_SINCE_EPOCH
+          return DAYS_SINCE_EPOCH;
         default:
           return null;
       }
@@ -103,93 +103,75 @@ public class OpenTxnsResponse implements org.apache.thrift.TBase<OpenTxnsRespons
   }
 
   // isset id assignments
+  private static final int __DAYSSINCEEPOCH_ISSET_ID = 0;
+  private byte __isset_bitfield = 0;
   public static final Map<_Fields, org.apache.thrift.meta_data.FieldMetaData> metaDataMap;
   static {
     Map<_Fields, org.apache.thrift.meta_data.FieldMetaData> tmpMap = new EnumMap<_Fields, org.apache.thrift.meta_data.FieldMetaData>(_Fields.class);
-    tmpMap.put(_Fields.TXN_IDS, new org.apache.thrift.meta_data.FieldMetaData("txn_ids", org.apache.thrift.TFieldRequirementType.REQUIRED, 
-        new org.apache.thrift.meta_data.ListMetaData(org.apache.thrift.protocol.TType.LIST, 
-            new org.apache.thrift.meta_data.FieldValueMetaData(org.apache.thrift.protocol.TType.I64))));
+    tmpMap.put(_Fields.DAYS_SINCE_EPOCH, new org.apache.thrift.meta_data.FieldMetaData("daysSinceEpoch", org.apache.thrift.TFieldRequirementType.REQUIRED, 
+        new org.apache.thrift.meta_data.FieldValueMetaData(org.apache.thrift.protocol.TType.I64)));
     metaDataMap = Collections.unmodifiableMap(tmpMap);
-    org.apache.thrift.meta_data.FieldMetaData.addStructMetaDataMap(OpenTxnsResponse.class, metaDataMap);
+    org.apache.thrift.meta_data.FieldMetaData.addStructMetaDataMap(Date.class, metaDataMap);
   }
 
-  public OpenTxnsResponse() {
+  public Date() {
   }
 
-  public OpenTxnsResponse(
-    List<Long> txn_ids)
+  public Date(
+    long daysSinceEpoch)
   {
     this();
-    this.txn_ids = txn_ids;
+    this.daysSinceEpoch = daysSinceEpoch;
+    setDaysSinceEpochIsSet(true);
   }
 
   /**
    * Performs a deep copy on <i>other</i>.
    */
-  public OpenTxnsResponse(OpenTxnsResponse other) {
-    if (other.isSetTxn_ids()) {
-      List<Long> __this__txn_ids = new ArrayList<Long>();
-      for (Long other_element : other.txn_ids) {
-        __this__txn_ids.add(other_element);
-      }
-      this.txn_ids = __this__txn_ids;
-    }
+  public Date(Date other) {
+    __isset_bitfield = other.__isset_bitfield;
+    this.daysSinceEpoch = other.daysSinceEpoch;
   }
 
-  public OpenTxnsResponse deepCopy() {
-    return new OpenTxnsResponse(this);
+  public Date deepCopy() {
+    return new Date(this);
   }
 
   @Override
   public void clear() {
-    this.txn_ids = null;
+    setDaysSinceEpochIsSet(false);
+    this.daysSinceEpoch = 0;
   }
 
-  public int getTxn_idsSize() {
-    return (this.txn_ids == null) ? 0 : this.txn_ids.size();
+  public long getDaysSinceEpoch() {
+    return this.daysSinceEpoch;
   }
 
-  public java.util.Iterator<Long> getTxn_idsIterator() {
-    return (this.txn_ids == null) ? null : this.txn_ids.iterator();
+  public void setDaysSinceEpoch(long daysSinceEpoch) {
+    this.daysSinceEpoch = daysSinceEpoch;
+    setDaysSinceEpochIsSet(true);
   }
 
-  public void addToTxn_ids(long elem) {
-    if (this.txn_ids == null) {
-      this.txn_ids = new ArrayList<Long>();
-    }
-    this.txn_ids.add(elem);
+  public void unsetDaysSinceEpoch() {
+    __isset_bitfield = EncodingUtils.clearBit(__isset_bitfield, __DAYSSINCEEPOCH_ISSET_ID);
   }
 
-  public List<Long> getTxn_ids() {
-    return this.txn_ids;
+  /** Returns true if field daysSinceEpoch is set (has been assigned a value) and false otherwise */
+  public boolean isSetDaysSinceEpoch() {
+    return EncodingUtils.testBit(__isset_bitfield, __DAYSSINCEEPOCH_ISSET_ID);
   }
 
-  public void setTxn_ids(List<Long> txn_ids) {
-    this.txn_ids = txn_ids;
-  }
-
-  public void unsetTxn_ids() {
-    this.txn_ids = null;
-  }
-
-  /** Returns true if field txn_ids is set (has been assigned a value) and false otherwise */
-  public boolean isSetTxn_ids() {
-    return this.txn_ids != null;
-  }
-
-  public void setTxn_idsIsSet(boolean value) {
-    if (!value) {
-      this.txn_ids = null;
-    }
+  public void setDaysSinceEpochIsSet(boolean value) {
+    __isset_bitfield = EncodingUtils.setBit(__isset_bitfield, __DAYSSINCEEPOCH_ISSET_ID, value);
   }
 
   public void setFieldValue(_Fields field, Object value) {
     switch (field) {
-    case TXN_IDS:
+    case DAYS_SINCE_EPOCH:
       if (value == null) {
-        unsetTxn_ids();
+        unsetDaysSinceEpoch();
       } else {
-        setTxn_ids((List<Long>)value);
+        setDaysSinceEpoch((Long)value);
       }
       break;
 
@@ -198,8 +180,8 @@ public class OpenTxnsResponse implements org.apache.thrift.TBase<OpenTxnsRespons
 
   public Object getFieldValue(_Fields field) {
     switch (field) {
-    case TXN_IDS:
-      return getTxn_ids();
+    case DAYS_SINCE_EPOCH:
+      return Long.valueOf(getDaysSinceEpoch());
 
     }
     throw new IllegalStateException();
@@ -212,8 +194,8 @@ public class OpenTxnsResponse implements org.apache.thrift.TBase<OpenTxnsRespons
     }
 
     switch (field) {
-    case TXN_IDS:
-      return isSetTxn_ids();
+    case DAYS_SINCE_EPOCH:
+      return isSetDaysSinceEpoch();
     }
     throw new IllegalStateException();
   }
@@ -222,21 +204,21 @@ public class OpenTxnsResponse implements org.apache.thrift.TBase<OpenTxnsRespons
   public boolean equals(Object that) {
     if (that == null)
       return false;
-    if (that instanceof OpenTxnsResponse)
-      return this.equals((OpenTxnsResponse)that);
+    if (that instanceof Date)
+      return this.equals((Date)that);
     return false;
   }
 
-  public boolean equals(OpenTxnsResponse that) {
+  public boolean equals(Date that) {
     if (that == null)
       return false;
 
-    boolean this_present_txn_ids = true && this.isSetTxn_ids();
-    boolean that_present_txn_ids = true && that.isSetTxn_ids();
-    if (this_present_txn_ids || that_present_txn_ids) {
-      if (!(this_present_txn_ids && that_present_txn_ids))
+    boolean this_present_daysSinceEpoch = true;
+    boolean that_present_daysSinceEpoch = true;
+    if (this_present_daysSinceEpoch || that_present_daysSinceEpoch) {
+      if (!(this_present_daysSinceEpoch && that_present_daysSinceEpoch))
         return false;
-      if (!this.txn_ids.equals(that.txn_ids))
+      if (this.daysSinceEpoch != that.daysSinceEpoch)
         return false;
     }
 
@@ -247,28 +229,28 @@ public class OpenTxnsResponse implements org.apache.thrift.TBase<OpenTxnsRespons
   public int hashCode() {
     HashCodeBuilder builder = new HashCodeBuilder();
 
-    boolean present_txn_ids = true && (isSetTxn_ids());
-    builder.append(present_txn_ids);
-    if (present_txn_ids)
-      builder.append(txn_ids);
+    boolean present_daysSinceEpoch = true;
+    builder.append(present_daysSinceEpoch);
+    if (present_daysSinceEpoch)
+      builder.append(daysSinceEpoch);
 
     return builder.toHashCode();
   }
 
-  public int compareTo(OpenTxnsResponse other) {
+  public int compareTo(Date other) {
     if (!getClass().equals(other.getClass())) {
       return getClass().getName().compareTo(other.getClass().getName());
     }
 
     int lastComparison = 0;
-    OpenTxnsResponse typedOther = (OpenTxnsResponse)other;
+    Date typedOther = (Date)other;
 
-    lastComparison = Boolean.valueOf(isSetTxn_ids()).compareTo(typedOther.isSetTxn_ids());
+    lastComparison = Boolean.valueOf(isSetDaysSinceEpoch()).compareTo(typedOther.isSetDaysSinceEpoch());
     if (lastComparison != 0) {
       return lastComparison;
     }
-    if (isSetTxn_ids()) {
-      lastComparison = org.apache.thrift.TBaseHelper.compareTo(this.txn_ids, typedOther.txn_ids);
+    if (isSetDaysSinceEpoch()) {
+      lastComparison = org.apache.thrift.TBaseHelper.compareTo(this.daysSinceEpoch, typedOther.daysSinceEpoch);
       if (lastComparison != 0) {
         return lastComparison;
       }
@@ -290,15 +272,11 @@ public class OpenTxnsResponse implements org.apache.thrift.TBase<OpenTxnsRespons
 
   @Override
   public String toString() {
-    StringBuilder sb = new StringBuilder("OpenTxnsResponse(");
+    StringBuilder sb = new StringBuilder("Date(");
     boolean first = true;
 
-    sb.append("txn_ids:");
-    if (this.txn_ids == null) {
-      sb.append("null");
-    } else {
-      sb.append(this.txn_ids);
-    }
+    sb.append("daysSinceEpoch:");
+    sb.append(this.daysSinceEpoch);
     first = false;
     sb.append(")");
     return sb.toString();
@@ -306,8 +284,8 @@ public class OpenTxnsResponse implements org.apache.thrift.TBase<OpenTxnsRespons
 
   public void validate() throws org.apache.thrift.TException {
     // check for required fields
-    if (!isSetTxn_ids()) {
-      throw new org.apache.thrift.protocol.TProtocolException("Required field 'txn_ids' is unset! Struct:" + toString());
+    if (!isSetDaysSinceEpoch()) {
+      throw new org.apache.thrift.protocol.TProtocolException("Required field 'daysSinceEpoch' is unset! Struct:" + toString());
     }
 
     // check for sub-struct validity
@@ -323,21 +301,23 @@ public class OpenTxnsResponse implements org.apache.thrift.TBase<OpenTxnsRespons
 
   private void readObject(java.io.ObjectInputStream in) throws java.io.IOException, ClassNotFoundException {
     try {
+      // it doesn't seem like you should have to do this, but java serialization is wacky, and doesn't call the default constructor.
+      __isset_bitfield = 0;
       read(new org.apache.thrift.protocol.TCompactProtocol(new org.apache.thrift.transport.TIOStreamTransport(in)));
     } catch (org.apache.thrift.TException te) {
       throw new java.io.IOException(te);
     }
   }
 
-  private static class OpenTxnsResponseStandardSchemeFactory implements SchemeFactory {
-    public OpenTxnsResponseStandardScheme getScheme() {
-      return new OpenTxnsResponseStandardScheme();
+  private static class DateStandardSchemeFactory implements SchemeFactory {
+    public DateStandardScheme getScheme() {
+      return new DateStandardScheme();
     }
   }
 
-  private static class OpenTxnsResponseStandardScheme extends StandardScheme<OpenTxnsResponse> {
+  private static class DateStandardScheme extends StandardScheme<Date> {
 
-    public void read(org.apache.thrift.protocol.TProtocol iprot, OpenTxnsResponse struct) throws org.apache.thrift.TException {
+    public void read(org.apache.thrift.protocol.TProtocol iprot, Date struct) throws org.apache.thrift.TException {
       org.apache.thrift.protocol.TField schemeField;
       iprot.readStructBegin();
       while (true)
@@ -347,20 +327,10 @@ public class OpenTxnsResponse implements org.apache.thrift.TBase<OpenTxnsRespons
           break;
         }
         switch (schemeField.id) {
-          case 1: // TXN_IDS
-            if (schemeField.type == org.apache.thrift.protocol.TType.LIST) {
-              {
-                org.apache.thrift.protocol.TList _list444 = iprot.readListBegin();
-                struct.txn_ids = new ArrayList<Long>(_list444.size);
-                for (int _i445 = 0; _i445 < _list444.size; ++_i445)
-                {
-                  long _elem446; // optional
-                  _elem446 = iprot.readI64();
-                  struct.txn_ids.add(_elem446);
-                }
-                iprot.readListEnd();
-              }
-              struct.setTxn_idsIsSet(true);
+          case 1: // DAYS_SINCE_EPOCH
+            if (schemeField.type == org.apache.thrift.protocol.TType.I64) {
+              struct.daysSinceEpoch = iprot.readI64();
+              struct.setDaysSinceEpochIsSet(true);
             } else { 
               org.apache.thrift.protocol.TProtocolUtil.skip(iprot, schemeField.type);
             }
@@ -374,62 +344,38 @@ public class OpenTxnsResponse implements org.apache.thrift.TBase<OpenTxnsRespons
       struct.validate();
     }
 
-    public void write(org.apache.thrift.protocol.TProtocol oprot, OpenTxnsResponse struct) throws org.apache.thrift.TException {
+    public void write(org.apache.thrift.protocol.TProtocol oprot, Date struct) throws org.apache.thrift.TException {
       struct.validate();
 
       oprot.writeStructBegin(STRUCT_DESC);
-      if (struct.txn_ids != null) {
-        oprot.writeFieldBegin(TXN_IDS_FIELD_DESC);
-        {
-          oprot.writeListBegin(new org.apache.thrift.protocol.TList(org.apache.thrift.protocol.TType.I64, struct.txn_ids.size()));
-          for (long _iter447 : struct.txn_ids)
-          {
-            oprot.writeI64(_iter447);
-          }
-          oprot.writeListEnd();
-        }
-        oprot.writeFieldEnd();
-      }
+      oprot.writeFieldBegin(DAYS_SINCE_EPOCH_FIELD_DESC);
+      oprot.writeI64(struct.daysSinceEpoch);
+      oprot.writeFieldEnd();
       oprot.writeFieldStop();
       oprot.writeStructEnd();
     }
 
   }
 
-  private static class OpenTxnsResponseTupleSchemeFactory implements SchemeFactory {
-    public OpenTxnsResponseTupleScheme getScheme() {
-      return new OpenTxnsResponseTupleScheme();
+  private static class DateTupleSchemeFactory implements SchemeFactory {
+    public DateTupleScheme getScheme() {
+      return new DateTupleScheme();
     }
   }
 
-  private static class OpenTxnsResponseTupleScheme extends TupleScheme<OpenTxnsResponse> {
+  private static class DateTupleScheme extends TupleScheme<Date> {
 
     @Override
-    public void write(org.apache.thrift.protocol.TProtocol prot, OpenTxnsResponse struct) throws org.apache.thrift.TException {
+    public void write(org.apache.thrift.protocol.TProtocol prot, Date struct) throws org.apache.thrift.TException {
       TTupleProtocol oprot = (TTupleProtocol) prot;
-      {
-        oprot.writeI32(struct.txn_ids.size());
-        for (long _iter448 : struct.txn_ids)
-        {
-          oprot.writeI64(_iter448);
-        }
-      }
+      oprot.writeI64(struct.daysSinceEpoch);
     }
 
     @Override
-    public void read(org.apache.thrift.protocol.TProtocol prot, OpenTxnsResponse struct) throws org.apache.thrift.TException {
+    public void read(org.apache.thrift.protocol.TProtocol prot, Date struct) throws org.apache.thrift.TException {
       TTupleProtocol iprot = (TTupleProtocol) prot;
-      {
-        org.apache.thrift.protocol.TList _list449 = new org.apache.thrift.protocol.TList(org.apache.thrift.protocol.TType.I64, iprot.readI32());
-        struct.txn_ids = new ArrayList<Long>(_list449.size);
-        for (int _i450 = 0; _i450 < _list449.size; ++_i450)
-        {
-          long _elem451; // optional
-          _elem451 = iprot.readI64();
-          struct.txn_ids.add(_elem451);
-        }
-      }
-      struct.setTxn_idsIsSet(true);
+      struct.daysSinceEpoch = iprot.readI64();
+      struct.setDaysSinceEpochIsSet(true);
     }
   }
 
